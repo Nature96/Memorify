1beec98be4eec1538b15e35cc9459e6fae5a6fde
nature96 <nisargntanna@gmail.com>
2023-10-06 17:19:52 -0700

    working code with refactored server code.

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios'; // Import Axios to make HTTP requests
+// import { useNavigate } from 'react-router-dom';
+// import axios from 'axios'; // Import Axios to make HTTP requests
 
 const SignUp = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  const handleSpotifyAuth = async () => {
-    try {
-      // Trigger Spotify authentication by making a request to your server's endpoint
-      await axios.get('http://localhost:5000/spotifyAuth');
-      navigate('/test'); // Redirect to home after initiating Spotify authentication
-    } catch (error) {
-      console.error('Error initiating Spotify authentication:', error);
-    }
-  };
+  // const handleSpotifyAuth = async () => {
+  //   try {
+  //     // Trigger Spotify authentication by making a request to your server's endpoint
+  //     await axios.get('http://localhost:5000/spotify/auth');
+  //     navigate('/test'); // Redirect to home after initiating Spotify authentication
+  //   } catch (error) {
+  //     console.error('Error initiating Spotify authentication:', error);
+  //   }
+  // };
 
   return (
     <div>
@@ -21,7 +21,7 @@ const SignUp = () => {
       <p>
         Click the link below to sign in with Spotify:
         <a
-          href="http://localhost:5000/spotifyAuth"
+          href="http://localhost:5000/spotify/auth"
           target="_blank" // Opens the link in a new tab/window
           rel="noopener noreferrer" // Recommended for security
         >

99789c4b1ecee7b85afb482e39adb7eb8522ec29
nature96 <nisargntanna@gmail.com>
2023-10-06 17:28:22 -0700

    Added environment variable support.

--- server/routes/spotify.js
+++ server/routes/spotify.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const SpotifyWebApi = require("spotify-web-api-node");
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: "0e75f419576246869c24069d7591c981",
-  clientSecret: "fb2607281fd142769b894e5b7b124f71",
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   redirectUri: "http://localhost:5000/spotify/callback",
 });
 
@@ -34,9 +34,9 @@ router.get("/callback", async (req, res) => {
     const { access_token, refresh_token } = data.body;
     spotifyApi.setAccessToken(access_token);
     spotifyApi.setRefreshToken(refresh_token);
-    res.redirect("http://localhost:3000/successful-sign-up"); // Update with your success route
+    res.redirect("http://localhost:3000/successful-sign-up");
   } catch (error) {
-    res.redirect("http://localhost:3000/unsuccessful-sign-up"); // Update with your failure route
+    res.redirect("http://localhost:3000/unsuccessful-sign-up");
   }
 });
 
